f514ab7889d594c79d58ecfe5e3ffcd2289f29c9
s-A013-08 <s-A013-08@CAMPUS.UEK.KRAKOW.PL>
2025-05-12 15:26:49 +0200

    2025-05-12 KK

---
+++ app/models.py
@@ -0,0 +1,39 @@
+class Product:
+    def __init__(self):
+        pass
+
+class Opinion:
+    selectors = {
+        "opinion_id": (None, "data-entry-id"),
+        "author": ("span.user-post__author-name",),
+        "recommendation": ("span.user-post__author-recomendation > em",),
+        "stars": ("span.user-post__score-count",),
+        "content_pl": ("div.user-post__text",),
+        "pros_pl": ("div.review-feature__item--positive", None, True),
+        "cons_pl": ("div.review-feature__item--negative", None, True),
+        "vote_yes": ("button.vote-yes","data-total-vote"),
+        "vote_no": ("button.vote-no","data-total-vote"),
+        "published": ("span.user-post__published > time:nth-child(1)","datetime"),
+        "purchased": ("span.user-post__published > time:nth-child(2)","datetime")
+    }
+
+    def __init__(self, opinion_id, author, recommendation, stars, content, pros, cons, vote_yes, vote_no, publish_date, purchase_date):
+        self.opinion_id = opinion_id
+        self.author = author
+        self.recommendation = recommendation
+        self.stars = stars
+        self.content = content
+        self.pros = pros
+        self.cons = cons
+        self.vote_yes = vote_yes
+        self.vote_no = vote_no
+        self.publish_date = publish_date
+        self.purchase_date = purchase_date
+    
+    def __str__(self):
+        return "\n".join([f"{key} : {getattr(self, key)}" for key in self.selectors.keys()])
+    
+    def __repr__(self):
+        return "Opinion("+"\n".join([f"{key}={str(getattr(self, key))}" for key in self.selectors.keys()])+")"
+    
+    
---
+++ app/views.py
@@ -0,0 +1,22 @@
+from app import app
+from flask import render_template
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/extract")
+def extract():
+    return render_template("extract.html")
+
+@app.route("/products")
+def products():
+    return render_template("products.html")
+
+@app.route("/product/<product_id>")
+def product(product_id):
+    return render_template("product.html", product_id=product_id)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
