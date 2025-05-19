a5d667cc31492fe7935aac17fc5e182f97fcc6b9
s-A013-08 <s-A013-08@CAMPUS.UEK.KRAKOW.PL>
2025-05-19 15:31:39 +0200

    2025-05-19 KK

--- app/models.py
+++ app/models.py
@@ -1,6 +1,70 @@
+import os
+import json
+import pandas as pd
+import numpy as np
+
 class Product:
-    def __init__(self):
-        pass
+    def __init__(self, product_id,  product_name, opinions, stats):
+        self.product_id = product_id,
+        self.product_name = product_name,
+        self.opinions = opinions,
+        self.stats = stats
+
+    def __str__(self):
+        return f"Product_id: {self.product_id}\nProduct_name: {self.product_name}\nOpinions: "+"\n\n".join([str(opinion) for opinion in self.opinions])+"\n"+json.dumps(self.stats, indent=4, ensure_ascii=False) + "\n"
+
+    def __repr__(self):
+        return f"Product(product_id={self.product_id}, product_name={self.product_name}, opinions=["+",".join(repr(opinion) for opinion in self.opinions) +f"], stats={self.stats})"
+    
+    def export_opinions(self):
+        if not os.path.exists("./app/data"):
+            os.mkdir("./app/data")
+        if not os.path.exists("./app/data/opinions"):
+            os.mkdir("./app/data/opinions")
+        with open(f".app/data/opinions/{self.product_id}.json", "w", encoding="UTF-8") as file:
+            json.dump([opinion.get_dict() for opinion in self.opinions], file, ensure_ascii=False, indent=4)
+
+    def export_info(self):
+        if not os.path.exists("./app/data"):
+            os.mkdir("./app/data")
+        if not os.path.exists("./app/data/products"):
+            os.mkdir("./app/data/products")
+        with open(f".app/data/products/{self.product_id}.json", "w", encoding="UTF-8") as file:
+            json.dump(self.get_dict(), file, ensure_ascii=False, indent=4)
+    
+    def get_dict(self):
+        return {"product_id": self.product_id,
+                "product_name": self.product_name,
+                "stats": self.stats}
+
+    def import_opinions(self):
+        with open(f".app/data/opinions/{self.product_id}.json", "r", encoding="UTF-8") as file:
+            onions = json.load(file)                          #Please dont correct this name (PS: i found it funny how onions and opinions sound similar)
+        for opinion in onions:
+            single_opinion = Opinion()
+            for key, value in opinion.items():
+                setattr(single_opinion, key, value)
+            self.opinions.append()
+
+    def import_info(self):
+        with open(f".app/data/products/{self.product_id}.json", "r", encoding="UTF-8") as file:
+            info = json.load(file)
+        self.product_id = info['product_id']
+        self.product_name = info['product_name']
+        self.opinions = info['opinions']
+        self.stats = info['stats']
+
+    def analyse(self):
+        opinions = pd.DataFrame.from_dict([opinion.get_dict() for opinion in self.opinions])
+        self.stats["opinions_count"] = opinions.shape[0]
+        self.stats["pros_count"] = opinions.pros_pl.astype(bool).sum()
+        self.stats["cons_count"] = opinions.cons_pl.astype(bool).sum()
+        self.stats["pros_cons_count"] = opinions.apply(lambda o: bool(o.pros_pl) and bool(o.cons_pl), axis=1).sum()
+        self.stats["average_rate"] = opinions.stars.mean()
+        self.stats["cons"] = opinions.cons_en.explode().value_counts()
+        self.stats["pros"] = opinions.pros_en.explode().value_counts()
+        self.stats["recommendations"] = opinions.recommendation.value_counts(dropna=False).reindex([False, True, np.nan], fill_value=0)
+        self.stats["stars"] = opinions.stars.value_counts().reindex(list(np.arrange(0,5.5,0.5)), fill_value=0)
 
 class Opinion:
     selectors = {
@@ -17,7 +81,7 @@ class Opinion:
         "purchased": ("span.user-post__published > time:nth-child(2)","datetime")
     }
 
-    def __init__(self, opinion_id, author, recommendation, stars, content, pros, cons, vote_yes, vote_no, publish_date, purchase_date):
+    def __init__(self, opinion_id="", author="", recommendation=False, stars=0.0, content="", pros=[], cons=[], vote_yes=0, vote_no=0, publish_date="", purchase_date=""):
         self.opinion_id = opinion_id
         self.author = author
         self.recommendation = recommendation
@@ -36,4 +100,6 @@ class Opinion:
     def __repr__(self):
         return "Opinion("+"\n".join([f"{key}={str(getattr(self, key))}" for key in self.selectors.keys()])+")"
     
+    def get_dict(self):
+        return {key: getattr(self, key) for key in self.selectors.keys()}
     
