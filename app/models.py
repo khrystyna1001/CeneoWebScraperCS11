e7e683495596d88d1576f1f2204ae9a00dc027b1
s-A013-07 <s-A013-07@CAMPUS.UEK.KRAKOW.PL>
2025-06-09 15:17:32 +0200

    09-06-2025 KK

--- app/models.py
+++ app/models.py
@@ -2,25 +2,9 @@ import os
 import json
 import pandas as pd
 import numpy as np
-<<<<<<< HEAD
-
-class Product:
-    def __init__(self, product_id,  product_name, opinions, stats):
-        self.product_id = product_id,
-        self.product_name = product_name,
-        self.opinions = opinions,
-        self.stats = stats
-
-    def __str__(self):
-        return f"Product_id: {self.product_id}\nProduct_name: {self.product_name}\nOpinions: "+"\n\n".join([str(opinion) for opinion in self.opinions])+"\n"+json.dumps(self.stats, indent=4, ensure_ascii=False) + "\n"
-
-    def __repr__(self):
-        return f"Product(product_id={self.product_id}, product_name={self.product_name}, opinions=["+",".join(repr(opinion) for opinion in self.opinions) +f"], stats={self.stats})"
-    
-=======
 import requests
 from bs4 import BeautifulSoup
-from config import headers
+from config_schema import headers
 from app.utils import extract_data, translate_data
 
 class Product:
@@ -65,55 +49,20 @@ class Product:
                 except TypeError:
                     next_page = None
 
->>>>>>> t_repo/main
     def export_opinions(self):
         if not os.path.exists("./app/data"):
             os.mkdir("./app/data")
         if not os.path.exists("./app/data/opinions"):
             os.mkdir("./app/data/opinions")
-<<<<<<< HEAD
-        with open(f".app/data/opinions/{self.product_id}.json", "w", encoding="UTF-8") as file:
-            json.dump([opinion.get_dict() for opinion in self.opinions], file, ensure_ascii=False, indent=4)
-=======
-        with open(f".app/data/opinions/{self.product_id}.json", "w", encoding="UTF-8") as jf:
+        with open(f"./app/data/opinions/{self.product_id}.json", "w", encoding="UTF-8") as jf:
             json.dump([opinion.transform_to_dict() for opinion in self.opinions], jf, ensure_ascii=False, indent=4)
->>>>>>> t_repo/main
 
     def export_info(self):
         if not os.path.exists("./app/data"):
             os.mkdir("./app/data")
         if not os.path.exists("./app/data/products"):
             os.mkdir("./app/data/products")
-<<<<<<< HEAD
-        with open(f".app/data/products/{self.product_id}.json", "w", encoding="UTF-8") as file:
-            json.dump(self.get_dict(), file, ensure_ascii=False, indent=4)
-    
-    def get_dict(self):
-        return {"product_id": self.product_id,
-                "product_name": self.product_name,
-                "stats": self.stats}
-
-    def import_opinions(self):
-        with open(f".app/data/opinions/{self.product_id}.json", "r", encoding="UTF-8") as file:
-            onions = json.load(file)                          #Please dont correct this name (PS: i found it funny how onions and opinions sound similar)
-        for opinion in onions:
-            single_opinion = Opinion()
-            for key, value in opinion.items():
-                setattr(single_opinion, key, value)
-            self.opinions.append()
-
-    def import_info(self):
-        with open(f".app/data/products/{self.product_id}.json", "r", encoding="UTF-8") as file:
-            info = json.load(file)
-        self.product_id = info['product_id']
-        self.product_name = info['product_name']
-        self.opinions = info['opinions']
-        self.stats = info['stats']
-
-    def analyse(self):
-        opinions = pd.DataFrame.from_dict([opinion.get_dict() for opinion in self.opinions])
-=======
-        with open(f".app/data/products/{self.product_id}.json", "w", encoding="UTF-8") as jf:
+        with open(f"./app/data/products/{self.product_id}.json", "w", encoding="UTF-8") as jf:
             json.dump(self.transform_to_dict(), jf, ensure_ascii=False, indent=4)
 
     def transform_to_dict(self):
@@ -140,23 +89,15 @@ class Product:
 
     def analyze(self):
         opinions = pd.DataFrame.from_dict([opinion.transform_to_dict() for opinion in self.opinions])
->>>>>>> t_repo/main
         self.stats["opinions_count"] = opinions.shape[0]
-        self.stats["pros_count"] = opinions.pros_pl.astype(bool).sum()
-        self.stats["cons_count"] = opinions.cons_pl.astype(bool).sum()
-        self.stats["pros_cons_count"] = opinions.apply(lambda o: bool(o.pros_pl) and bool(o.cons_pl), axis=1).sum()
-        self.stats["average_rate"] = opinions.stars.mean()
-<<<<<<< HEAD
-        self.stats["cons"] = opinions.cons_en.explode().value_counts()
-        self.stats["pros"] = opinions.pros_en.explode().value_counts()
-        self.stats["recommendations"] = opinions.recommendation.value_counts(dropna=False).reindex([False, True, np.nan], fill_value=0)
-        self.stats["stars"] = opinions.stars.value_counts().reindex(list(np.arrange(0,5.5,0.5)), fill_value=0)
-=======
-        self.stats["pros"] = opinions.pros_en.explode().value_counts()
-        self.stats["cons"] = opinions.cons_en.explode().value_counts()
-        self.stats["recommendations"] = opinions.recommendation.value_counts(dropna=False).reindex([False, True, np.nan], fill_value=0)
-        self.stats["stars"] = opinions.stars.value_counts().reindex(list(np.arange(0,5.5,0.5)), fill_value=0)
->>>>>>> t_repo/main
+        self.stats["pros_count"] = int(opinions.pros_pl.astype(bool).sum())
+        self.stats["cons_count"] = int(opinions.cons_pl.astype(bool).sum())
+        self.stats["pros_cons_count"] = int(opinions.apply(lambda o: bool(o.pros_pl) and bool(o.cons_pl), axis=1).sum())
+        self.stats["average_rate"] = float(opinions.stars.mean())
+        self.stats["pros"] = opinions.pros_pl.explode().value_counts().to_dict()
+        self.stats["cons"] = opinions.cons_pl.explode().value_counts().to_dict()
+        self.stats["recommendations"] = opinions.recommendation.value_counts(dropna=False).reindex([False, True, np.nan], fill_value=0).to_dict()
+        self.stats["stars"] = opinions.stars.value_counts().reindex(list(np.arange(0,5.5,0.5)), fill_value=0).to_dict()
 
 class Opinion:
     selectors = {
@@ -170,11 +111,10 @@ class Opinion:
         "vote_yes": ("button.vote-yes","data-total-vote"),
         "vote_no": ("button.vote-no","data-total-vote"),
         "published": ("span.user-post__published > time:nth-child(1)","datetime"),
-<<<<<<< HEAD
-        "purchased": ("span.user-post__published > time:nth-child(2)","datetime")
-=======
         "purchased": ("span.user-post__published > time:nth-child(2)","datetime"),
->>>>>>> t_repo/main
+        "content_en": (),
+        "pros_en": (),
+        "cons_en": ()
     }
 
     def __init__(self, opinion_id="", author="", recommendation=False, stars=0.0, content="", pros=[], cons=[], vote_yes=0, vote_no=0, publish_date="", purchase_date=""):
@@ -182,37 +122,19 @@ class Opinion:
         self.author = author
         self.recommendation = recommendation
         self.stars = stars
-<<<<<<< HEAD
-        self.content = content
-        self.pros = pros
-        self.cons = cons
-=======
         self.content_pl = content
         self.pros_pl = pros
         self.cons_pl = cons
->>>>>>> t_repo/main
         self.vote_yes = vote_yes
         self.vote_no = vote_no
         self.publish_date = publish_date
         self.purchase_date = purchase_date
-<<<<<<< HEAD
-    
-    def __str__(self):
-        return "\n".join([f"{key} : {getattr(self, key)}" for key in self.selectors.keys()])
-    
-    def __repr__(self):
-        return "Opinion("+"\n".join([f"{key}={str(getattr(self, key))}" for key in self.selectors.keys()])+")"
-    
-    def get_dict(self):
-        return {key: getattr(self, key) for key in self.selectors.keys()}
-    
-=======
         self.content_en = ""
         self.pros_en = []
         self.cons_en = []
     
     def __str__(self):
-        return ("\n".join([f"{key}: {getattr(self, key)}" for key in self.selectors.keys()]))+f"content_en: {self.content_en}\npros_en: {self.pros_en}\ncons_en: {self.cons_en}"
+        return ("\n".join([f"{key}: {getattr(self, key)}" for key in self.selectors.keys()]))
     
     def __repr__(self):
         return "Opinion("+", ".join([f"{key}={str(getattr(self, key))}" for key in self.selectors.keys()])+")"
@@ -239,4 +161,3 @@ class Opinion:
     def transform_to_dict(self):
         return {key: getattr(self, key) for key in self.selectors.keys()}
     
->>>>>>> t_repo/main
--- app/views.py
+++ app/views.py
@@ -1,21 +1,15 @@
+import os
+import json
 from app import app
-<<<<<<< HEAD
-from flask import render_template
-=======
 from flask import render_template, redirect, url_for, request
 from app.forms import ExtractForm
 from app.models import Product
->>>>>>> t_repo/main
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 @app.route("/extract")
-<<<<<<< HEAD
-def extract():
-    return render_template("extract.html")
-=======
 def render_form():
     form = ExtractForm()
     return render_template("extract.html", form=form)
@@ -35,15 +29,39 @@ def extract():
         form.product_id.errors.append('There is no product for provided id or product has no opinions')
         return render_template('extract.html', form=form)
     return render_template('extract.html', form=form)
->>>>>>> t_repo/main
 
 @app.route("/products")
 def products():
-    return render_template("products.html")
+    products_list = []
+    products_dir = "./app/data/products"
+    if os.path.exists(products_dir):
+        for filename in os.listdir(products_dir):
+            if filename.endswith(".json"):
+                product_id = filename.replace(".json", "")
+                product_filepath = os.path.join(products_dir, filename)
+                try:
+                    with open(product_filepath, "r", encoding="UTF-8") as f:
+                        product_data = json.load(f)
+                        products_list.append({
+                            'product_id': product_data.get('product_id'),
+                            'product_name': product_data.get('product_name')
+                        })
+                except Exception as e:
+                    print(f"Error reading prodcut file {filename}: {e}")
+    return render_template("products.html", products=products_list)
 
 @app.route("/product/<product_id>")
 def product(product_id):
-    return render_template("product.html", product_id=product_id)
+    product = Product(product_id)
+    try:
+        product.import_info()
+        product.import_opinions()
+    except FileNotFoundError:
+        return "Product data not found.", 404
+    except Exception as e:
+        return f"An error occurred: {e}", 500
+
+    return render_template("product.html", product=product)
 
 @app.route("/about")
 def about():
